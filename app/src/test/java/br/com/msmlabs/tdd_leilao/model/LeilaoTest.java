5259c4622865160e2e1cb7db8e7a5fc832107d76
Denis Ferreira <denisferreira93@hotmail.com>
2022-06-02 11:57:33 -0300

    1.1.2 - Introdução aos testes automatizados - Aula 5: Testando o maior lance

--- app/src/test/java/br/com/msmlabs/tdd_leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/msmlabs/tdd_leilao/model/LeilaoTest.java
@@ -1,9 +1,12 @@
 package br.com.msmlabs.tdd_leilao.model;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
 
-public class LeilaoTest extends TestCase {
+import org.junit.Test;
 
+public class LeilaoTest {
+
+    @Test
     public void testGetDescricao() {
         // criar cenário de teste
         Leilao console = new Leilao("Console");
@@ -13,5 +16,19 @@ public class LeilaoTest extends TestCase {
 
         // testar resultado esperado
         assertEquals("Console", descricaoDevolvida);
+    }
+
+    @Test
+    public void getMaiorLance(){
+        // criar cenário de teste
+        Leilao console = new Leilao("Console");
+        console.propoe(new Lance(new Usuario("Alex"), 200.0));
+
+        // executar ação esperada
+        double maiorLanceDevolvido = console.getMaiorLance();
+
+        // testar resultado esperado
+        assertEquals(200.0, maiorLanceDevolvido, 0.0001);
+
     }
 }
